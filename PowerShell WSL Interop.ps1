f567f724d25d68660bd646dfdbdfcdaf67cade68
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2019-09-18 18:40:55 -0700

    Fixed #7 as well as added additional Format-WSLArgument call to the arguments to wslpath which is necessary to escape parentheses and other things.

--- PowerShell WSL Interop.ps1
+++ PowerShell WSL Interop.ps1
@@ -48,7 +48,7 @@ function Import-WSLCommands() {
         for (`$i = 0; `$i -lt `$args.Length; `$i++) {
             # If a path is absolute with a qualifier (e.g. C:), run it through wslpath to map it to the appropriate mount point.
             if (Split-Path `$args[`$i] -IsAbsolute -ErrorAction Ignore) {
-                `$args[`$i] = Format-WSLArgument (wsl.exe wslpath (`$args[`$i] -replace "\\", "/"))
+                `$args[`$i] = Format-WSLArgument (wsl.exe wslpath (Format-WSLArgument (`$args[`$i] -replace "\\", "/")))
             # If a path is relative, the current working directory will be translated to an appropriate mount point, so just format it.
             } elseif (Test-Path -IsValid `$args[`$i] -ErrorAction Ignore) {
                 `$args[`$i] = Format-WSLArgument (`$args[`$i] -replace "\\", "/")
@@ -141,7 +141,11 @@ function Import-WSLCommands() {
 
     # Helper function to escape characters in arguments passed to WSL that would otherwise be misinterpreted.
     function global:Format-WSLArgument([string]$arg, [bool]$interactive) {
-        return ($arg -replace " ", "\ ") -replace "([()])", ('\$1', '\`$1')[$interactive]
+        if ($interactive -and $arg.Contains(" ")) {
+            return "'$arg'"
+        } else {
+            return ($arg -replace " ", "\ ") -replace "([()])", ('\$1', '`$1')[$interactive]
+        }
     }
 }
 
