feced0793d722da418a6365d85141c9e6e9bd34a
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2019-09-19 21:34:05 -0700

    Removed unnecessary Format-WSLArgument call.

--- PowerShell WSL Interop.ps1
+++ PowerShell WSL Interop.ps1
@@ -48,7 +48,7 @@ function Import-WSLCommands() {
         for (`$i = 0; `$i -lt `$args.Count; `$i++) {
             # If a path is absolute with a qualifier (e.g. C:), run it through wslpath to map it to the appropriate mount point.
             if (Split-Path `$args[`$i] -IsAbsolute -ErrorAction Ignore) {
-                `$args[`$i] = Format-WSLArgument (wsl.exe wslpath (Format-WSLArgument (`$args[`$i] -replace "\\", "/")))
+                `$args[`$i] = Format-WSLArgument (wsl.exe wslpath (`$args[`$i] -replace "\\", "/"))
             # If a path is relative, the current working directory will be translated to an appropriate mount point, so just format it.
             } elseif (Test-Path -IsValid `$args[`$i] -ErrorAction Ignore) {
                 `$args[`$i] = Format-WSLArgument (`$args[`$i] -replace "\\", "/")
