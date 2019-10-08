4c244ae6216028abc37eb1fc5a9526523840cde1
Tomoaki Goto <elessar869@gmail.com>
2019-10-08 23:15:35 +0900

    [CommentFix] Fix comment indentation

--- WslInterop.psm1
+++ WslInterop.psm1
@@ -1,46 +1,43 @@
 function global:Import-WslCommand() {
     <#
     .SYNOPSIS
-    Import Linux commands into the session as PowerShell functions with argument completion.
+        Import Linux commands into the session as PowerShell functions with argument completion.
 
     .DESCRIPTION
-    WSL enables calling Linux commands directly within PowerShell via wsl.exe (e.g. wsl ls). While more convenient
-    than a full context switch into WSL, it has the following limitations:
+        WSL enables calling Linux commands directly within PowerShell via wsl.exe (e.g. wsl ls). While more convenient
+        than a full context switch into WSL, it has the following limitations:
+            * Prefixing commands with wsl is tedious and unnatural
+            * Windows paths passed as arguments don't often resolve due to backslashes being interpreted as escape characters rather than directory separators
+            * Windows paths passed as arguments don't often resolve due to not being translated to the appropriate mount point within WSL
+            * Default parameters defined in WSL login profiles with aliases and environment variables aren’t honored
+            * Linux path completion is not supported
+            * Command completion is not supported
+            * Argument completion is not supported
+            
+        This function addresses these issues in the following ways:    
+            * By creating PowerShell function wrappers for commands, prefixing them with wsl is no longer necessary
+            * By identifying path arguments and converting them to WSL paths, path resolution is natural and intuitive as it translates seamlessly between Windows and WSL paths
+            * Default parameters are supported by $WslDefaultParameterValues similar to $PSDefaultParameterValues
+            * Command completion is enabled by PowerShell's command completion
+            * Argument completion is enabled by registering an ArgumentCompleter that shims bash's programmable completion
 
-    * Prefixing commands with wsl is tedious and unnatural
-    * Windows paths passed as arguments don't often resolve due to backslashes being interpreted as escape characters rather than directory separators
-    * Windows paths passed as arguments don't often resolve due to not being translated to the appropriate mount point within WSL
-    * Default parameters defined in WSL login profiles with aliases and environment variables aren’t honored
-    * Linux path completion is not supported
-    * Command completion is not supported
-    * Argument completion is not supported
-    
-    This function addresses these issues in the following ways:
-    
-    * By creating PowerShell function wrappers for commands, prefixing them with wsl is no longer necessary
-    * By identifying path arguments and converting them to WSL paths, path resolution is natural and intuitive as it translates seamlessly between Windows and WSL paths
-    * Default parameters are supported by $WslDefaultParameterValues similar to $PSDefaultParameterValues
-    * Command completion is enabled by PowerShell's command completion
-    * Argument completion is enabled by registering an ArgumentCompleter that shims bash's programmable completion
+        The commands can receive both pipeline input as well as their corresponding arguments just as if they were native to Windows.
 
-    The commands can receive both pipeline input as well as their corresponding arguments just as if they were native to Windows.
+        Additionally, they will honor any default parameters defined in a hash table called $WslDefaultParameterValues similar to $PSDefaultParameterValues. For example:
+            * $WslDefaultParameterValues["grep"] = "-E"
+            * $WslDefaultParameterValues["less"] = "-i"
+            * $WslDefaultParameterValues["ls"] = "-AFh --group-directories-first"
 
-    Additionally, they will honor any default parameters defined in a hash table called $WslDefaultParameterValues similar to $PSDefaultParameterValues. For example:
+        If you use aliases or environment variables within your login profiles to set default parameters for commands, define a hash table called $WslDefaultParameterValues within
+        your PowerShell profile and populate it as above for a similar experience.
 
-    * $WslDefaultParameterValues["grep"] = "-E"
-    * $WslDefaultParameterValues["less"] = "-i"
-    * $WslDefaultParameterValues["ls"] = "-AFh --group-directories-first"
-
-    If you use aliases or environment variables within your login profiles to set default parameters for commands, define a hash table called $WslDefaultParameterValues within
-    your PowerShell profile and populate it as above for a similar experience.
-
-    The import of these functions replaces any PowerShell aliases that conflict with the commands.
+        The import of these functions replaces any PowerShell aliases that conflict with the commands.
 
     .PARAMETER Command
-    Specifies the commands to import.
+        Specifies the commands to import.
 
     .EXAMPLE
-    Import-WslCommand "awk", "emacs", "grep", "head", "less", "ls", "man", "sed", "seq", "ssh", "tail", "vim"
+        Import-WslCommand "awk", "emacs", "grep", "head", "less", "ls", "man", "sed", "seq", "ssh", "tail", "vim"
     #>
 
     [CmdletBinding()]
