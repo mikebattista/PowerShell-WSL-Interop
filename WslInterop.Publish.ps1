ac106b0c193b00ff13ce7ce7a6e360f96fb9bf34
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2020-06-03 20:59:27 -0700

    Added validation to the Publish script.

--- WslInterop.Publish.ps1
+++ WslInterop.Publish.ps1
@@ -3,8 +3,11 @@ $moduleName = $module.Name
 $moduleVersion = $module.Version.ToString()
 $modulePath = "$(($Env:PSModulePath -split ';')[0])\$moduleName\$moduleVersion"
 
+[version]::new($moduleVersion).CompareTo([version]::new((Find-Module $moduleName).Version)) | Should -Be 1
+
 Remove-Item $modulePath -Recurse -Force -ErrorAction Ignore 
 New-Item $modulePath -ItemType Directory -Force | Out-Null
 Copy-Item .\* $modulePath -Recurse -Exclude .github, *.Publish.ps1, *.Tests.ps1
+Get-ChildItem $modulePath -Force | Select-Object -ExpandProperty Name | Should -BeExactly "LICENSE", "README.md", "WslInterop.psd1", "WslInterop.psm1"
 
 Publish-Module -Name $moduleName -RequiredVersion $moduleVersion -NuGetApiKey $args[0] -Confirm
