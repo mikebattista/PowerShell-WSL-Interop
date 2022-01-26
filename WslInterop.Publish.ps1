f2c1b06564e256c005ca2fbcd5148fb8d2a9e622
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-01-25 17:40:15 -0800

    Updated to the latest version of PowerShellGet.

--- WslInterop.Publish.ps1
+++ WslInterop.Publish.ps1
@@ -1,13 +1,14 @@
 $module = Test-ModuleManifest .\*.psd1
 $moduleName = $module.Name
 $moduleVersion = $module.Version.ToString()
-$modulePath = "$(($Env:PSModulePath -split ';')[0])\$moduleName\$moduleVersion"
+$modulePath = "$Env:TEMP\$(New-Guid)\$moduleName"
 
-[version]::new($moduleVersion).CompareTo([version]::new((Find-Module $moduleName).Version)) | Should -Be 1
+[version]::new($moduleVersion).CompareTo([version]::new((Find-PSResource $moduleName).Version)) | Should -Be 1
 
-Remove-Item $modulePath -Recurse -Force -ErrorAction Ignore 
 New-Item $modulePath -ItemType Directory -Force | Out-Null
 Copy-Item .\* $modulePath -Recurse -Exclude .github, *.Publish.ps1, *.Tests.ps1
 Get-ChildItem $modulePath -Force | Select-Object -ExpandProperty Name | Should -BeExactly "LICENSE", "README.md", "WslInterop.psd1", "WslInterop.psm1"
 
-Publish-Module -Name $moduleName -RequiredVersion $moduleVersion -NuGetApiKey $args[0] -Confirm
+Publish-PSResource -Path $modulePath -Repository PSGallery -ApiKey $args[0] -Confirm
+
+Remove-Item $modulePath -Recurse -Force -ErrorAction Ignore
